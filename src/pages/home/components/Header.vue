f030b4034b3bf636fd3a1234fce81aee7c7377ec
kaiaZero <kaiaLu225@gmail.com>
2019-03-18 16:00:10 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class='header'>
+    <div class='header-left'>
+      <div class='iconfont back-icon'>&#xe624;</div>
+    </div>
+    <div class='header-input'>
+      <span class='iconfont'>&#xe632;</span>
+      输入城市/景点/游玩主题</div>
+    <div class='header-right'>
+      城市
+      <span class='iconfont arrow-icon'>&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~@/assets/styles/variables.styl'
+  .header
+    display:flex
+    line-height:0.86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      width:0.64rem
+      float:left
+      .back-icon
+        font-size: .4rem
+        text-align: center
+    .header-input
+      flex:1
+      height:.62rem
+      line-height: .62rem
+      background-color: #fff
+      border-radius: .1rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      color: #ccc
+    .header-right
+      width:1.24rem
+      float:right
+      text-align: center
+      .arrow-icon
+        font-size: .24rem
+        margin-left: -0.04rem
+</style>
