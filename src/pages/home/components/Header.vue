1a221d61052ed5810f1910a0381ca91f22c8791f
kaiaZero <kaiaLu225@gmail.com>
2019-03-18 23:28:28 +0800

    add swiper

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -20,7 +20,7 @@ export default {
 </script>
 
 <style lang='stylus' scoped>
-  @import '~@/assets/styles/variables.styl'
+  @import '~styles/variables.styl'
   .header
     display:flex
     line-height:0.86rem
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="wrapper">
+  <swiper :options="swiperOption" >
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key= "item.id">
+        <img class="swiper-image" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: require('@/assets/images/slide11.jpg')
+      }, {
+        id: '002',
+        imgUrl: require('@/assets/images/slide12.jpg')
+      }, {
+        id: '003',
+        imgUrl: require('@/assets/images/slide13.jpg')
+      }, {
+        id: '004',
+        imgUrl: require('@/assets/images/slide14.jpg')
+      }, {
+
+        id: '005',
+        imgUrl: require('@/assets/images/slide15.jpg')
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff
+.wrapper
+  width:100%
+  height:0
+  overflow: hidden
+  padding-bottom: 26.66%
+  background-color: #eee
+  .swiper-image
+    width:100%
+</style>
