8764104c8616331f7df91df8e56565331957e4d8
kaiaZero <kaiaLu225@gmail.com>
2019-03-19 09:41:57 +0800

    add icon section

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class= 'icons'>
+    <div class= 'icon'>
+      <div class= 'icon-image'>
+        <img src= 'https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'/>
+      </div>
+      <p>热门景点</p>
+    </div>
+    <div class= 'icon'>
+      <div class= 'icon-image'>
+        <img src= 'https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'/>
+      </div>
+      <p>热门景点</p>
+    </div>
+    <div class= 'icon'>
+      <div class= 'icon-image'>
+        <img src= 'https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'/>
+      </div>
+      <p>热门景点</p>
+    </div>
+    <div class= 'icon'>
+      <div class= 'icon-image'>
+        <img src= 'https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'/>
+      </div>
+      <p>热门景点</p>
+    </div>
+    <div class= 'icon'>
+      <div class= 'icon-image'>
+        <img src= 'https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'/>
+      </div>
+      <p>热门景点</p>
+    </div>
+    <div class= 'icon'>
+      <div class= 'icon-image'>
+        <img src= 'https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'/>
+      </div>
+      <p>热门景点</p>
+    </div>
+    <div class= 'icon'>
+      <div class= 'icon-image'>
+        <img src= 'https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'/>
+      </div>
+      <p>热门景点</p>
+    </div>
+    <div class= 'icon'>
+      <div class= 'icon-image'>
+        <img src= 'https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png'/>
+      </div>
+      <p>热门景点</p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/variables.styl'
+  .icons
+    width: 100%
+    height: 0
+    padding-bottom: 50%
+    overflow: hidden
+    .icon
+      position: relative
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      float: left
+      overflow: hidden
+      .icon-image
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        img
+          display: block
+          margin: 0 auto
+          height: 100%
+      p
+        position: absolute
+        bottom: 0
+        left: 0
+        right: 0
+        height:0.44rem
+        line-height: .44rem
+        color: $darkTextColor
+        text-align: center
+</style>
