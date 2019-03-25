d3af09ffe763ded9896155686ee97d8f824fc3ac
kaiaZero <kaiaLu225@gmail.com>
2019-03-25 22:31:33 +0800

    finish city list

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class ="banner">
+    <img class="banner-img"
+         src="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang='stylus' scoped>
+  .banner
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    background: green
+</style>
