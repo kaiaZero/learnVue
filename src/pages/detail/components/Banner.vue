211c68b1ac2c9cb3df5d587dbf6a3cc032beb2ba
kaiaZero <kaiaLu225@gmail.com>
2019-03-26 19:11:15 +0800

    reset

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class ="banner">
+    <div class="wrapper">
+      <swiper :options="swiperOption" v-if="showSwiper">
+        <!-- slides -->
+        <swiper-slide v-for="item of list" :key= "item.id">
+          <!-- <img class="swiper-image" :src="getImgSrc(item)"> -->
+          <img class="banner-img" :src="require(`@/assets/images/${item.imgUrl}`)">
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang='stylus' scoped>
+  .banner
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    background: green
+</style>
